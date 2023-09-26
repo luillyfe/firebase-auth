498cb52773a7c400556cf28a44bd7f35334c030a
Fermin Blanco <luillyfe89@gmail.com>
2023-09-26 02:11:28 -0500

    chores: adding tailwindcss library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1>Firebase Application</h1>;
+  return <h1 className="text-3xl font-bold underline">Firebase Application</h1>;
 }
 
 export default App;
